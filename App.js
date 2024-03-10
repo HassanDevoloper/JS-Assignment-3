d1652027b5e098db122852cb005a2ddb3f6f6bbc
HassanDevoloper <161868607+HassanDevoloper@users.noreply.github.com>
2024-03-09 19:33:07 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+//Q:1. Write a program to take “city” name as input from user. If
+//user enters “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+                           // --------Ans---------
+
+     //chap 9 to 11
+
+ //Q:1.Write a program to take “city” name as input from user. If 
+//user enters “Karachi”,welcome the user like this: 
+//“Welcome to city of lights”
+
+//var cityName = prompt("Enter your city name");
+
+//if (cityName.toLowerCase() === "karachi") {
+           // console.log("welcome to the city of light") }
+  
+  //Q:2.Write a program to take “gender” as input from user. If the
+//user is male, give the message: Good Morning Sir. If the
+//user is female, give the message: Good Morning Ma’am.
+
+                         // -------Ans--------
+
+ // var Gender = prompt("Enter Gender male/female");
+  // if (Gender === "male") {
+           //console.log ("Good morning sir");
+  // }
+  //else if (Gender === "female") {
+           // console.log ("Good morning ma'am");
+  //  } 
+  // else {
+   //  console.log ("Invalid input.please Enter gender male or female");
+  // } 
+  
+   //Q:3. Write a program to take input color of road traffic signal
+  //from the user & show the message according to this table:
+
+                           // -------Ans---------
+
+ var signalColor = prompt("Enter the name of Traffic signal color red/yellow/green");                     
+
+ if (signalColor === "red") {
+    console.log("Must Stop");
+ }
+  else if (signalColor === "yellow") {
+    console.log("Ready to move");
+  }
+  else if (signalColor === "green") {
+    console.log("move now");
+  }
+  else {
+     console.log("Invalid color.please enter signal color red/yellow/green");
+  }
+          
